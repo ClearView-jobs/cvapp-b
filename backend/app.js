55d85c694d1811a6513138f54540a0f006f53b87
Deeswoc <47671781+Deeswoc@users.noreply.github.com>
2020-07-26 21:31:35 -0500

    added firebase to the project and added made a small test endpoint for the companies

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,12 @@
+const 
+  admin = require('firebase-admin'),
+  serviceAccount = require('./Keys/ServiceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore(); 
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -21,6 +30,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/companies', (req, res) => {
+  db.collection('companies').get().then((doc) => {
+    if(!doc.exists){
+      console.log('no record');
+      res.sendStatus(404);
+    }
+    let companies = [];
+    doc.forEach(company=>{
+      companies.push(company.data())
+    })
+    res.json(companies);
+  }).catch((err)=>{
+    console.log(err);
+    res.sendStatus(500);
+  })
+});
+
+app.post('/companies', (req, res) => {
+  let company = {
+    name: req.body.name,
+    number_of_employees: req.body.number_of_employees
+  }
+  //db.collection('sample').doc('company').add(company).then(() => {
+  db.collection('companies').add(company).then(() => {
+    console.log("quote was written to database");
+    res.sendStatus(201)
+  }).catch((err)=>{
+    console.log(err);
+    res.sendStatus(500);
+  })
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

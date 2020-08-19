713b5287ea555a21905071737b38ad6cab0eb481
Shakeane <shakeanehinds97@gmail.com>
2020-08-19 02:44:46 -0500

    added env variables

--- firebase-settings.js
+++ firebase-settings.js
@@ -1,11 +1,15 @@
-const 
-  admin = require('firebase-admin'),
-  serviceAccount = require('./config/ServiceAccountKey.json');
+const admin = require("firebase-admin"),
+  serviceAccount = require("./config/ServiceAccountKey.json");
+require("dotenv").config();
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
-const db = admin.firestore(); 
+const db = admin.firestore();
 
 module.exports = db;
